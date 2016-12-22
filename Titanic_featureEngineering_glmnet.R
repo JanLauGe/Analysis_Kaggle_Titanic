e8accc20e32f13f4017a49e712f4ca8e8fccb2eb
Laurens Geffert <laurensgeffert@gmail.com>
2016-12-22 17:13:16 +0000

    updated modelling
    Changed evaluation metric
    added tuning grid
    bugfix data munging

--- Titanic_featureEngineering_glmnet.R
+++ Titanic_featureEngineering_glmnet.R
@@ -16,7 +16,6 @@ d.init.predict <- read.csv("../input/test.csv",
 # DATA MUNGING ----
 # bind two datasets together to format them the same way
 d.all <- cbind(d.init.predict, "Survived" = factor(NA, levels = c(0,1))) %>% rbind(d.init.model)
-attach(d.all)
 
 # Re-format data
 GetNames <- function(full.names){
@@ -26,25 +25,26 @@ GetNames <- function(full.names){
   names.split2 <- strsplit(names.rest, split = "\\.")
   names.title <- lapply(names.split2, FUN = function(x) {x[[1]]}) %>% unlist()
   names.title <- revalue(as.factor(names.title), 
-                         replace = c("the Countess"="Posh", "Sir"="Posh", "Lady"="Posh",
-                                     "Capt"="Posh", "Col"="Posh", "Major"="Posh",
-                                     "Jonkheer"="Posh", "Don"="Posh","Dona"="Posh",
-                                     "Dr"="Posh", "Rev"="Posh",
-                                     "Ms"="Mrs", "Mlle"="Miss", "Mme"="Mrs"))
+                            replace = c("the Countess"="Posh", "Sir"="Posh", "Lady"="Posh",
+                                        "Capt"="Posh", "Col"="Posh", "Major"="Posh",
+                                        "Jonkheer"="Posh", "Don"="Posh","Dona"="Posh",
+                                        "Dr"="Posh", "Rev"="Posh",
+                                        "Ms"="Mrs", "Mlle"="Miss", "Mme"="Mrs"))
   names.first <- lapply(names.split2, FUN = function(x) {ifelse(length(x) > 2, paste(x[[2]], x[[3]]), x[[2]])}) %>% unlist()
   split.names <- data.frame("Title" = names.title, "First" = names.first, "Last" = names.last)
   return(split.names)
 }
-d.all <- cbind(d.all, GetNames(Name))
+d.all <- cbind(d.all, GetNames(d.all$Name))
+attach(d.all)
 
 # fill missing age values with inferred information
 d.all[is.na(Embarked),"Embarked"] <- "S"
 d.all$Age <- ifelse(!is.na(Age), Age,
-                    ifelse(is.na(Age) & Title == "Posh", mean(Age[Title=="Posh"], na.rm = T),
-                           ifelse(is.na(Age) & Title == "Mr", mean(Age[Title=="Mr"], na.rm = T),
-                                  ifelse(is.na(Age) & Title == "Mrs", mean(Age[Title=="Mrs"], na.rm = T),
-                                         ifelse(is.na(Age) & Title == "Miss", mean(Age[Title=="Miss"], na.rm = T),
-                                                ifelse(is.na(Age) & Title == "Master", mean(Age[Title=="Master"], na.rm = T), NA))))))
+       ifelse(is.na(Age) & Title == "Posh", mean(Age[Title=="Posh"], na.rm = T),
+              ifelse(is.na(Age) & Title == "Mr", mean(Age[Title=="Mr"], na.rm = T),
+                     ifelse(is.na(Age) & Title == "Mrs", mean(Age[Title=="Mrs"], na.rm = T),
+                            ifelse(is.na(Age) & Title == "Miss", mean(Age[Title=="Miss"], na.rm = T),
+                                   ifelse(is.na(Age) & Title == "Master", mean(Age[Title=="Master"], na.rm = T), NA))))))
 
 # get deck where available
 GetDeck <- function(cabin){
@@ -74,27 +74,40 @@ d.model <- d.all[!is.na(Survived),c("Survived","Title","Last","Sex","Age","Famil
 d.predict <- d.all[is.na(Survived),c("Title","Last","Sex","Age","Family","SibSp","Parch","Pclass","Embarked")]
 detach(d.all)
 
+head(d.all)
+summary(d.all)
+
 #I ran a basic glmnet with regularisation. Alpha and lambda were optimised using the caret package. I evaluated the performance with each tuning parameter using three-fold crossvalidation.
 # MODELLING ----
 # create training and test data partition
-data <- d.model
-
+data <- d.model %>%
+    mutate(Survived = as.factor(Survived)) %>%
+    mutate(Survived = fct_recode(Survived, 'survived' = '0', 'died' = '1'))
+    
 # build a glmnet model
-# car.train.grid <- expand.grid(alpha, lambda)
-car.train.control <- trainControl(method = "repeatedcv",
-                                  number = 3,
-                                  repeats = 3,
-                                  summaryFunction = defaultSummary)
-car.train <- train(Survived ~ 1 + .,
-                   data = data,
-                   method = "glmnet",
-                   family = "binomial",
-                   metric = "Accuracy",
-                   #tuneGrid = car.train.grid,
-                   trControl = car.train.control)
+car.train.grid <- expand.grid(
+    alpha = c(0.1, 0.2, 0.3, 0.5, 0.7, 1), 
+    lambda = c(0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1))
+car.train.control <- trainControl(
+    method = "repeatedcv",
+    number = 10,
+    repeats = 3,
+    classProbs = TRUE,
+    summaryFunction = twoClassSummary)
+car.train <- train(
+    Survived ~ 1 + .,
+    data = data,
+    method = "glmnet",
+    family = "binomial",
+    metric = "ROC",
+    tuneGrid = car.train.grid,
+    trControl = car.train.control)
+
+car.train
+plot(car.train)
 
 #Finally, I used the best fitted model for a prediction on the test dataset.
 # PREDICTION ----
 out <- predict(car.train, d.predict)
-out <- cbind("PassengerId" = d.init.predict$PassengerId, "Survived" = as.numeric(test.pred)-1)
-write.csv(out, file = "../output/predict.csv", row.names=F)
+out <- cbind("PassengerId" = d.init.predict$PassengerId, "Survived" = as.numeric(out)-1)
+write.csv(out, file = "predict.csv", row.names=F, quote=FALSE)
